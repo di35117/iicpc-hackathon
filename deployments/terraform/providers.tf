1080108ae44ec8d58b2c2d8b3a1f68f78d4ba6bb
dishank choudhury <dishankchoudhury@gmail.com>
2026-06-14 07:50:36 +0530

    horizontal scaling started

---
+++ deployments/terraform/providers.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = ">= 1.5.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.23"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1" # Mumbai Region
+  default_tags {
+    tags = {
+      Project     = "iicpc-benchmarking-platform"
+      Environment = "production"
+      ManagedBy   = "terraform"
+    }
+  }
+}
